7eb2b3a371f01d1849651b545391fd57421f128d
Valik0805 <kirillotohin@yandex.ru>
2020-08-10 14:39:36 +0700

    Добавьте файлы проекта.

---
+++ Queue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue <Person> person = new Queue<Person>();
+
+            person.Enqueue(new Person() { Name = "Polina" });
+            person.Enqueue(new Person() { Name = "Evgeny" });
+            person.Enqueue(new Person() { Name = "Pavel" });
+
+            Person PerosonFromQueue = person.Peek();
+            Console.WriteLine(PerosonFromQueue.Name + "\n");
+
+            foreach(Person p in person)
+            {
+                Console.WriteLine(p.Name);
+            }
+
+        }
+    }
+}
+//Dequeue: извлекает и возвращает первый элемент очереди
+
+//Enqueue: добавляет элемент в конец очереди
+
+//Peek: просто возвращает первый элемент из начала очереди без его удаления
